415350d45cc4df0c1557c6784ccca4b6628c72da
정핸모 <gusah009@naver.com>
2024-03-27 16:01:54 +0900

    initial project

---
+++ zk-recipe/src/main/kotlin/org/example/zkrecipe/ZkRecipeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.zkrecipe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZkRecipeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZkRecipeApplication>(*args)
+}
---
+++ zk-recipe/src/test/kotlin/org/example/zkrecipe/ZkRecipeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.zkrecipe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZkRecipeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
